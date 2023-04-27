5b32f7f2c8cee03fbe7c4a0585d91fc51039b4f9
Curaca <curaca492@gmail.com>
2023-04-27 13:18:39 -0500

    Insert right and left rotation.

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        AVL tree = new AVL();
+        tree.insertNode(15, "a");
+        tree.insertNode(6, "a");
+        tree.insertNode(7, "a");
+        tree.insertNode(10, "a");
+        tree.insertNode(17, "a");
+
+
+        System.out.println(tree.longestPath());
+        System.out.println(tree.balancedFactor(tree.getRoot()));
+
     }
 }
