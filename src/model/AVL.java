95006a02c8584b3a2741dad9234f45a31c530914
Curaca <curaca492@gmail.com>
2023-04-27 12:03:15 -0500

    Adding AVL

---
+++ src/model/AVL.java
@@ -0,0 +1,124 @@
+public class AVL <K extends Comparable,V> {
+    private  Node<K,V> root;
+
+    /**
+     * Consiste en una busqueda del padre al cual se va a conectar el nodo a insertar.
+     * @param key
+     * @param value
+     */
+    public void insertNode(K key,  V value){
+
+        Node nodeToInsert = new Node<>(key,value);
+
+        if (root == null) {
+            root = nodeToInsert;
+            return;
+        }
+
+        Node father = null;
+        Node<K,V> temporalFather = root;
+
+        while (temporalFather != null){ // Este ciclo lo uso para obtener el padre al cual debo ingresar el nodo.
+            father = temporalFather;
+            // El orden de comparación es relavante.
+            if (key.compareTo(temporalFather.getKey()) < 0) temporalFather = temporalFather.getLeft();
+            else temporalFather = temporalFather.getRight();
+        }
+
+        if (nodeToInsert.getKey().compareTo(father.getKey()) < 0 ) father.setLeft(nodeToInsert);
+        else father.setRight(nodeToInsert);
+        nodeToInsert.setParent(father);
+    }
+
+    public Node<K,V> successor(Node<K,V> node){
+
+        if (root == null) return null;
+        if (node.getRight() != null) return minimum(node.getRight());
+
+        Node<K,V> father = node.getParent();
+        Node<K,V> temporal = node;
+
+        if (father == null) return null;
+
+        while(father.getRight() == temporal ){
+            temporal = father;
+            father = father.getParent();
+            if (father== null) return null;
+        }
+        return father;
+    }
+
+    public void delete(K key){
+        if (root == null) {
+            System.out.println("Arbol vacio");
+            return;
+        }
+
+        Node<K,V> toDelete = search(key);
+        if (toDelete == null ) {
+            System.out.println("No se encontró nodo a eliminar");
+            return;
+        }
+        Node<K,V> temporalToDelete;
+        Node<K,V> connection;
+
+        if (toDelete.getRight() == null || toDelete.getLeft() == null ) temporalToDelete = toDelete;
+        else temporalToDelete = successor(toDelete);
+
+        if (temporalToDelete.getLeft() != null) connection = temporalToDelete.getLeft();
+        else connection = temporalToDelete.getRight();
+
+        if (connection != null) connection.setParent(temporalToDelete.getParent());
+
+        if (temporalToDelete.getParent() == null){
+            root.setKey(connection.getKey());
+            root.setValue(connection.getValue());
+        }else{
+            if (temporalToDelete == temporalToDelete.getParent().getLeft())
+                temporalToDelete.getParent().setLeft(connection);
+            else temporalToDelete.getParent().setRight(connection);
+        }
+
+        if (temporalToDelete != toDelete){
+            toDelete.setKey(temporalToDelete.getKey());
+            toDelete.setValue(temporalToDelete.getValue());
+        }
+    }
+
+    public Node<K,V> search(K key){
+        if (root == null) return null;
+        else return search(key,root);
+    }
+
+    public Node<K,V> search(K key,  Node<K,V> current){
+        if (current.getKey().compareTo(key) == 0) return current;
+        if (key.compareTo(current.getKey()) < 0 ) return search(key,current.getLeft());
+        else return search(key,current.getRight());
+    }
+
+    public Node<K,V> minimum (){
+        if (root == null) return null;
+        else return minimum(root);
+    }
+
+    public Node<K,V> minimum(Node<K,V > current){
+        if (current.getLeft() == null) return current;
+        return minimum(current.getLeft());
+    }
+    public String printTree(){
+        if (root == null) return "Raiz vacia";
+        else return printTreeInternal(root);
+    }
+
+    private String printTreeInternal(Node current){
+        if (current == null)return "";
+        return printTreeInternal(current.getLeft()) + current.getKey() + ", " + printTreeInternal(current.getRight());
+    }
+    public Node<K, V> getRoot() {
+        return root;
+    }
+    public void setRoot(Node<K, V> root) {
+        this.root = root;
+    }
+}
+
--- src/model/Node.java
+++ src/model/Node.java
@@ -1,32 +1,37 @@
-public class Node <K extends Comparable<K>,T>{
+public class Node <K extends Comparable, V>{
 
-    private  Node<K,T> parent;
-    private  Node<K,T> right;
-    private  Node<K,T> left;
+    private  Node<K, V> parent;
+    private  Node<K, V> right;
+    private  Node<K, V> left;
     private K key;
-    private T value;
+    private V value;
 
-    public Node<K, T> getParent() {
+    public Node(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public Node<K, V> getParent() {
         return parent;
     }
 
-    public void setParent(Node<K, T> parent) {
+    public void setParent(Node<K, V> parent) {
         this.parent = parent;
     }
 
-    public Node<K, T> getRight() {
+    public Node<K, V> getRight() {
         return right;
     }
 
-    public void setRight(Node<K, T> right) {
+    public void setRight(Node<K, V> right) {
         this.right = right;
     }
 
-    public Node<K, T> getLeft() {
+    public Node<K, V> getLeft() {
         return left;
     }
 
-    public void setLeft(Node<K, T> left) {
+    public void setLeft(Node<K, V> left) {
         this.left = left;
     }
 
@@ -38,11 +43,11 @@ public class Node <K extends Comparable<K>,T>{
         this.key = key;
     }
 
-    public T getValue() {
+    public V getValue() {
         return value;
     }
 
-    public void setValue(T value) {
+    public void setValue(V value) {
         this.value = value;
     }
 }
